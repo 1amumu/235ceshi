7f7920906fdc54f5772dd84f102ca7d2eeade9d3
ZkiKz <1276791513@qq.com>
2025-05-07 10:53:05 +0800

    web1

---
+++ web-mobile/cocos-js/wait-for-ammo-instantiation.js
@@ -0,0 +1,13 @@
+System.register(['./instantiated-1fea2f2a.js'], function (exports) {
+	'use strict';
+	return {
+		setters: [function (module) {
+			exports('default', module.hw);
+		}],
+		execute: function () {
+
+
+
+		}
+	};
+});
